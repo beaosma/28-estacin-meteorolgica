b52ddef85f6080764629ebf43f6b24fd63321feb
BEATRIZ <99761092+beaosma@users.noreply.github.com>
2023-01-11 19:26:50 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,16 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showString("Temperatura satélite" + receivedNumber)
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Temperatura base" + input.temperature())
+})
+radio.onReceivedString(function (receivedString) {
+    radio.sendNumber(input.temperature())
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("temperatura satélite")
+})
+radio.setGroup(1)
+basic.forever(function () {
+	
+})
